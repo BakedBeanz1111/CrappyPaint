0c5bcd471ec790b541783dcefc16aa3e33bc7d58
Amad Ali <amad.ali@sandc.com>
2020-06-25 21:15:18 -0500

    Started Shape object file

--- src/model/Shape.java
+++ src/model/Shape.java
@@ -4,6 +4,23 @@ import java.awt.*;
 
 public class Shape {
 
-    //What makes a Shape Object
-    private
+    //At its core, what makes a shape a shape on a 2D rendering plane?
+    private Point startPoint;
+    private Point endPoint;
+
+    //get methods
+    public Point getStartPoint() {
+        return startPoint;
+    }
+    public Point getEndPoint() {
+        return endPoint;
+    }
+
+    //set methods
+    public void setStartPoint(Point startPoint) {
+        this.startPoint = startPoint;
+    }
+    public void setEndPoint(Point endPoint) {
+        this.endPoint = endPoint;
+    }
 }
