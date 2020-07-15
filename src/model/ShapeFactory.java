7decd3dd6d53fac6ee83a706d5e2f3569886da89
Amad Ali <amad.ali@sandc.com>
2020-07-14 23:26:22 -0500

    i guess this is a factory class

--- src/model/ShapeFactory.java
+++ src/model/ShapeFactory.java
@@ -2,5 +2,43 @@ package model;
 
 //https://www.tutorialspoint.com/design_pattern/factory_pattern.htm
 
+import model.interfaces.IApplicationState;
+
 public class ShapeFactory {
+
+    public IApplicationState applicationState;
+    public Shape shape;
+    public ShapeList shapeList;
+
+    public ShapeFactory(IApplicationState applicationState, Shape shape, ShapeList shapeList) {
+
+        this.applicationState = applicationState;
+        this.shape = shape;
+        this.shapeList = shapeList;
+    }
+
+    public void createRectangle() {
+
+        if(applicationState.getActiveShapeType().toString().equals("RECTANGLE")) {
+
+            System.out.println(("RECTANGLE!!!"));
+        }
+    }
+
+    public void createEllipse() {
+
+        if(applicationState.getActiveShapeType().toString().equals("ELLIPSE")) {
+
+            System.out.println(("ELLIPSE!!!"));
+        }
+    }
+
+    public  void createTriangle() {
+
+        if(applicationState.getActiveShapeType().toString().equals("TRIANGLE")) {
+
+            System.out.println("TRIANGLE!!!");
+        }
+    }
+
 }
